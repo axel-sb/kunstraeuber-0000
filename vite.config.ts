4267f22d6062801499f6837f5447c096ce8bc8f5
axel-sb <40129856+axel-sb@users.noreply.github.com>
2025-03-07 08:20:07 +0100

    assert vite.config with 'satifies ViteUserConfig'

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
+import { resolve } from 'path'
 import { reactRouter } from '@react-router/dev/vite'
 import { sentryVitePlugin } from '@sentry/vite-plugin'
-import { resolve } from 'path'
 import { envOnlyMacros } from 'vite-env-only'
-/* import { defineConfig, type ViteUserConfig } from 'vitest/config'
-import { reactRouterDevTools } from 'react-router-devtools'
-import tsconfigPaths from 'vite-tsconfig-paths' */
+import { type ViteUserConfig } from 'vitest/config'
 
 const MODE = process.env.NODE_ENV
 
@@ -86,4 +84,4 @@ export default {
 			all: true,
 		},
 	},
-}
+} satisfies ViteUserConfig
